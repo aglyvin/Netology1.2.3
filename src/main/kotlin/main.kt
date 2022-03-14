0b17675c657247bdd4798b0144d1fa13d386403e
glyvin <glyvin@pointcad1.onmicrosoft.com>
2022-03-14 17:46:20 +0300

    changes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main() {
+    val maxDicount = 0.05
+    val standartDiscount = 10000
+    val spending = 90000
+    val isLoyal = true
+    val orderPrice = 5000
+    var finalPrice = 0;
+
+    val discount = if(spending <= 1000100 && spending >= 100100) orderPrice * (1 - maxDicount) else ()
+    if (spending <= 1000100) finalPrice = (Int)(orderPrice * (1 - maxDicount))
 
 }
